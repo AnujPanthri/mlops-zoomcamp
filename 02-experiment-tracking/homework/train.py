48cb5b15db2a61b4daba44e27db7d217fbbd0a82
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2024-05-21 08:50:13 +0000

    finished homework

---
+++ 02-experiment-tracking/homework/train.py
@@ -0,0 +1,39 @@
+import os
+import pickle
+import click
+
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+    mlflow.autolog()
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        print("val_rmse:",rmse)
+
+if __name__ == '__main__':
+    # mlflow.set_tracking_uri()
+    # print(mlflow.get_tracking_uri())
+    run_train()
--- README.md
+++ README.md
@@ -7,8 +7,15 @@
     - [X] added training notebook
     - [X] done homework
 
-- [ ] 02-experiment-tracking
+- [X] 02-experiment-tracking
     - [X] setup mlflow
     - [X] do experiment tracking
     - [X] Model tracking
     - [X] Model Registry
+    - [X] Launch tracking server:- mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root artifacts
+
+
+
+## Tools
+
+- Tool to download github sub directory: https://download-directory.github.io/
