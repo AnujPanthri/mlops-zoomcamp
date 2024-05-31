3a67a237953363f44b16f79f77def399527072fe
AnujPanthri <50690518+AnujPanthri@users.noreply.github.com>
2024-05-31 06:34:09 +0000

    homework done

--- 03-orchestration/mlops/mlops/homework_03/data_exporters/train.py
+++ 03-orchestration/mlops/mlops/homework_03/data_exporters/train.py
@@ -31,48 +31,48 @@ def train_model(
     categorical = ["PULocationID", "DOLocationID"]
     numerical = []
 
-    # # train a dict vectorizer
-    # dv = DictVectorizer()
-    # train_dicts = train_df[categorical+numerical].to_dict(orient="records")
-    # X_train = dv.fit_transform(train_dicts)
-    # y_train = train_df.duration
+    # train a dict vectorizer
+    dv = DictVectorizer()
+    train_dicts = train_df[categorical+numerical].to_dict(orient="records")
+    X_train = dv.fit_transform(train_dicts)
+    y_train = train_df.duration
 
     # setup mlflow 
     
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_tracking_uri("http://mlflow:5000")
     mlflow.set_experiment("yellow-taxi-duration")
-    # mlflow.set_experiment("random-forest-hyperopt")
+    
     # print(mlflow.get_tracking_uri())
-    # with mlflow.start_run():
-    #     mlflow.log_param("model","linear_regression")
-    #     # train model
-    #     model = LinearRegression()
-    #     model.fit(X_train, y_train)
+    with mlflow.start_run():
+        mlflow.log_param("model","linear_regression")
+        # train model
+        model = LinearRegression()
+        model.fit(X_train, y_train)
 
-    #     # evalulate model
-    #     y_pred = model.predict(X_train)
-    #     rmse = mean_squared_error(y_train,y_pred,squared=False)
-    #     mse = mean_squared_error(y_train,y_pred)
-    #     intercept = model.intercept_
+        # evalulate model
+        y_pred = model.predict(X_train)
+        rmse = mean_squared_error(y_train,y_pred,squared=False)
+        mse = mean_squared_error(y_train,y_pred)
+        intercept = model.intercept_
 
-    #     metrics={
-    #         "rmse":rmse,
-    #         "mse":mse,
-    #         "intercept":intercept,
-    #     }
+        metrics={
+            "rmse":rmse,
+            "mse":mse,
+            "intercept":intercept,
+        }
 
-    #     os.makedirs("preprocessing",exists_ok=True)
-    #     with open("preprocessing/preprocessing.b",'wb') as f:
-    #         pickle.dump(dv,f)
+        os.makedirs("preprocessing",exist_ok=True)
+        with open("preprocessing/preprocessing.b",'wb') as f:
+            pickle.dump(dv,f)
 
-    #     mlflow.log_metrics(metrics)
-    #     mlflow.sklearn.log_model(model,"model")
-    #     mlflow.log_artifact(
-    #         "preprocessing/preprocessing.b",
-    #         artifact_path="preprocessing/preprocessing.b",
+        mlflow.log_metrics(metrics)
+        mlflow.sklearn.log_model(model,"model")
+        mlflow.log_artifact(
+            "preprocessing/preprocessing.b",
+            artifact_path="model",
 
-    #     )
+        )
         
 
 
-    # return dv, model, metrics
+    return dv, model, metrics
--- 03-orchestration/mlops/mlops/homework_03/scratchpads/curious_butterfly.py
+++ 03-orchestration/mlops/mlops/homework_03/scratchpads/curious_butterfly.py
@@ -1,5 +1,4 @@
 import mlflow
 
-# mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_tracking_uri("http://0.0.0.0:5000")
+mlflow.set_tracking_uri("http://mlflow:5000")
 mlflow.set_experiment("yellow-taxi-duration")
